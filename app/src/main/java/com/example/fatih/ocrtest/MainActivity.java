466e7fbb0cbd56ed2ea96ed29c3a3274e14795b3
Fatih Barmanbay <fthbrmnby@gmail.com>
2015-11-29 17:07:00 +0200

    Parsed JSON and printed on screen

--- app/src/main/java/com/example/fatih/ocrtest/MainActivity.java
+++ app/src/main/java/com/example/fatih/ocrtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fatih.ocrtest;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -8,14 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_INTENT_CLICK=302;
     String path="Deneme";
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView resultView = (TextView) findViewById(R.id.result);
         if (resultCode == RESULT_OK)
         {
             if (requestCode == MY_INTENT_CLICK)
@@ -53,7 +61,13 @@ public class MainActivity extends AppCompatActivity {
                 path = selectedImagePath;
                 Log.e("Image File Path", path);
 
-                new LongOperation().execute();
+                try {
+                    resultView.setText(new LongOperation().execute().get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -72,9 +86,21 @@ public class MainActivity extends AppCompatActivity {
                 multipart.addFormField("param_name_3", "param_value");
                 multipart.addFilePart("file", new File(path));
                 List<String> response = multipart.finish(); // response from server.
+                String res="";
+                JSONObject jObj;
+
                 for (int i=0;i<response.size();i++){
                     Log.e("SONUC:::",response.get(i));
+                    res += response.get(i);
                 }
+                jObj = new JSONObject(res);
+                JSONArray aJsonArray = jObj.getJSONArray("ParsedResults");
+                String parsedText="";
+                for (int i =0; i<aJsonArray.length(); i++){
+                    JSONObject c = aJsonArray.getJSONObject(i);
+                    parsedText = c.getString("ParsedText");
+                }
+                return parsedText;
             }catch (Exception e){
                 Log.e("ERROR ON BUILDING URL",e.toString());
             }
@@ -84,11 +110,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             Log.e("###Arkaplan İşlemi##","İşlem Bitti");
+            if (pDialog.isShowing())
+                pDialog.dismiss();
         }
 
         @Override
         protected void onPreExecute() {
-            Log.e("###Arkaplan İşlemi##","İşlem Başladı");
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please Wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
         }
 
         @Override
